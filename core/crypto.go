203c053b8df8cf29c8a07dc70c7a102d850e675f
Nizar Gharbi <nizar.gha25@gmail.com>
2018-03-07 23:51:22 -0500

    Change errors, Add primitives, and test

--- core/crypto.go
+++ core/crypto.go
@@ -3,10 +3,13 @@ package core
 import (
 	"crypto"
 	"crypto/cipher"
+	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 	"encoding/base64"
 	"errors"
 	"golang.org/x/crypto/chacha20poly1305"
+	"io"
 )
 
 /*
@@ -24,13 +27,113 @@ const (
 /*
 	Errors
 */
-const (
-	base64DecodeError      = "Error decoding base64 payload."
-	invalidNonceError      = "Invalid nonce provided."
-	noSymKeyFoundError     = "No symmetric key passed the challenge."
-	payloadDecryptionError = "Payload Decryption failed."
-	payloadDecodeError     = "Payload Decoding failed."
-)
+var base64DecodeError error = errors.New("Error decoding base64.")
+var invalidNonceError error = errors.New("Invalid nonce provided.")
+var invalidSymmetricKeyError error = errors.New("Invalid key provided.")
+var aeadCreationError error = errors.New("Aead creation failed.")
+var noSymmetricKeyFoundError error = errors.New("No symmetric key passed the challenge.")
+var signError error = errors.New("Signing failed.")
+var asymmetrictEncryptionError error = errors.New("Asymmetric encryption failed.")
+var asymmetrictDecryptionError error = errors.New("Asymmetric decryption failed.")
+var symmetrictDecryptionError error = errors.New("Ssymmetric decryption failed.")
+var payloadDecryptionError error = errors.New("Payload decryption failed.")
+var payloadDecodeError error = errors.New("Payload decoding failed.")
+
+/*
+	Primitives
+*/
+
+var rng io.Reader = rand.Reader
+
+func Base64EncodeToString(src []byte) string {
+	return base64.StdEncoding.EncodeToString(src)
+}
+
+func Base64DecodeString(src string) (res []byte, err error) {
+	res, err = base64.StdEncoding.DecodeString(src)
+	if err != nil {
+		return nil, base64DecodeError
+	}
+	return
+}
+
+func ValidateNonce(nonce []byte) error {
+	if len(nonce) != SymmetricNonceSize {
+		return invalidNonceError
+	}
+	return nil
+}
+
+func ValidateSymmetricKey(key []byte) error {
+	if len(key) != SymmetricKeySize {
+		return invalidSymmetricKeyError
+	}
+	return nil
+}
+
+func Hash(plaintext []byte) []byte {
+	hashed := sha256.Sum256(plaintext)
+	return hashed[:]
+}
+
+func Sign(key *rsa.PrivateKey, plaintext []byte) ([]byte, error) {
+	signature, err := rsa.SignPKCS1v15(rng, key, HashingAlgorithm, plaintext[:])
+	if err != nil {
+		return nil, signError
+	}
+	return signature, nil
+}
+
+func Verify(key *rsa.PublicKey, plaintext []byte, signature []byte) bool {
+	err := rsa.VerifyPKCS1v15(key, HashingAlgorithm, plaintext[:], signature)
+	return err == nil
+}
+
+func AsymmetricEncrypt(key *rsa.PublicKey, plaintext []byte) ([]byte, error) {
+	ciphertext, err := rsa.EncryptPKCS1v15(rng, key, plaintext)
+	if err != nil {
+		return nil, asymmetrictEncryptionError
+	}
+	return ciphertext, nil
+}
+
+func AsymmetricDecrypt(key *rsa.PrivateKey, ciphertext []byte) ([]byte, error) {
+	plaintext, err := rsa.DecryptPKCS1v15(rng, key, ciphertext)
+	if err != nil {
+		return nil, asymmetrictDecryptionError
+	}
+	return plaintext, nil
+}
+
+func NewAead(key []byte) (cipher.AEAD, error) {
+	aead, err := chacha20poly1305.New(key)
+	if err != nil {
+		return nil, aeadCreationError
+	}
+	return aead, nil
+}
+
+func SymmetricEncrypt(aead cipher.AEAD, dst []byte, nonce []byte, plaintext []byte) []byte {
+	return aead.Seal(
+		dst,
+		nonce,
+		plaintext,
+		[]byte{},
+	)
+}
+
+func SymmetricDecrypt(aead cipher.AEAD, dst []byte, nonce []byte, ciphertext []byte) ([]byte, error) {
+	plaintext, err := aead.Open(
+		dst,
+		nonce,
+		ciphertext,
+		[]byte{},
+	)
+	if err != nil {
+		return nil, symmetrictDecryptionError
+	}
+	return plaintext, nil
+}
 
 /*
 	Temporary decryption
@@ -41,9 +144,9 @@ func (op *TemporaryEncryptedOperation) Decrypt(asymKey *rsa.PrivateKey) (*Perman
 	}
 
 	// Base64 decode payload
-	payloadBytes, err := base64.StdEncoding.DecodeString(op.Payload)
+	payloadBytes, err := Base64DecodeString(op.Payload)
 	if err != nil {
-		return nil, errors.New(base64DecodeError)
+		return nil, err
 	}
 
 	// Decrypt payload if encrypted
@@ -51,45 +154,47 @@ func (op *TemporaryEncryptedOperation) Decrypt(asymKey *rsa.PrivateKey) (*Perman
 	if op.Encryption.Encrypted {
 
 		// Check nonce
-		symKeyNonceBytes, err := base64.StdEncoding.DecodeString(op.Encryption.Nonce)
-		if err != nil ||
-			len(symKeyNonceBytes) != SymmetricNonceSize {
-			return nil, errors.New(invalidNonceError)
+		symKeyNonceBytes, err := Base64DecodeString(op.Encryption.Nonce)
+		if err == nil {
+			if ValidateNonce(symKeyNonceBytes) != nil {
+				return nil, invalidNonceError
+			}
 		}
 
 		// Find a symmetric key that passes challenge
 		for symKeyCipher, symKeyChallenge := range op.Encryption.Challenges {
 			// Decode symmetric key ciphertext
-			symKeyCipherBytes, err := base64.StdEncoding.DecodeString(symKeyCipher)
+			symKeyCipherBytes, err := Base64DecodeString(symKeyCipher)
 			if err != nil {
 				continue
 			}
 
 			// Decrypt symmetric key
-			symKeyPlainBytes, symKeyDecryptErr := rsa.DecryptPKCS1v15(nil, asymKey, symKeyCipherBytes)
-			if symKeyDecryptErr != nil ||
-				len(symKeyPlainBytes) != SymmetricKeySize {
-				continue
+			symKeyPlainBytes, err := AsymmetricDecrypt(asymKey, symKeyCipherBytes)
+			if err == nil {
+				if ValidateSymmetricKey(symKeyPlainBytes) != nil {
+					continue
+				}
 			}
 
-			// Make symmetric key
-			symKeyAead, symKeyAeadErr := chacha20poly1305.New(symKeyPlainBytes)
+			// Make Aead
+			symKeyAead, symKeyAeadErr := NewAead(symKeyPlainBytes)
 			if symKeyAeadErr != nil {
 				panic("Aead creation failed despite valid key and nonce size")
 			}
 
 			// Decode challenge
-			symKeyChallengeBytes, err := base64.StdEncoding.DecodeString(symKeyChallenge)
+			symKeyChallengeBytes, err := Base64DecodeString(symKeyChallenge)
 			if err != nil {
 				continue
 			}
 
 			// Decrypt challenge
-			decryptedChallenge, decryptedChallengeErr := symKeyAead.Open(
+			decryptedChallenge, decryptedChallengeErr := SymmetricDecrypt(
+				symKeyAead,
 				symKeyChallengeBytes[:0],
 				symKeyNonceBytes,
 				symKeyChallengeBytes,
-				[]byte{},
 			)
 
 			// Test if decrypted challenge is correct
@@ -102,28 +207,26 @@ func (op *TemporaryEncryptedOperation) Decrypt(asymKey *rsa.PrivateKey) (*Perman
 
 		// No symmetric keys worked
 		if aead == nil {
-			return nil, errors.New(noSymKeyFoundError)
+			return nil, noSymmetricKeyFoundError
 		}
 
 		// Decrypt payload
-		payloadDecryptedBytes, payloadDecryptErr := aead.Open(
+		payloadBytes, err = SymmetricDecrypt(
+			aead,
 			payloadBytes[:0],
 			symKeyNonceBytes,
 			payloadBytes,
-			[]byte{},
 		)
-		if payloadDecryptErr != nil {
-			return nil, errors.New(payloadDecryptionError)
+		if err != nil {
+			return nil, err
 		}
-
-		payloadBytes = payloadDecryptedBytes
 	}
 
 	// Decode payload
 	var decodedOp PermanentEncryptedOperation
 	payloadDecodeErr := decodedOp.Decode(payloadBytes)
 	if payloadDecodeErr != nil {
-		return nil, errors.New(payloadDecodeError)
+		return nil, payloadDecodeError
 	}
 
 	return &decodedOp, nil
