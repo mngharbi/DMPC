0343ab2689b450f22ce9a06baee569154518b6cf
Nizar Gharbi <nizar.gha25@gmail.com>
2018-04-16 21:02:39 -0400

    Add generic Locker interface and functor generators in core

--- core/sync.go
+++ core/sync.go
@@ -5,14 +5,33 @@
 package core
 
 import (
+	"github.com/mngharbi/memstore"
 	"sort"
 )
 
+/*
+	Lock type alias
+	false = read lock
+	true = write lock
+*/
 type LockType bool
 
-const WriteLockType LockType = true
 const ReadLockType LockType = false
+const WriteLockType LockType = true
 
+/*
+	Locking type alias
+	false = unlocking
+	true = locking
+*/
+type LockingType bool
+
+const Unlocking LockingType = false
+const Locking LockingType = true
+
+/*
+	Lock need definition and sanitization
+*/
 type LockNeed struct {
 	LockType LockType
 	Id       string
@@ -80,6 +99,9 @@ func sanitizeUnlockNeeds(lockNeeds []LockNeed) []LockNeed {
 	return sanitizedLockNeeds
 }
 
+/*
+	Generic Locking function
+*/
 func Lock(doLock func(string, LockType) bool, doUnlock func(string, LockType) bool, lockNeeds []LockNeed) bool {
 	// Sanitize lock needs to avoid deadlocks
 	sanitizedLockNeeds := sanitizeLockNeeds(lockNeeds)
@@ -108,6 +130,9 @@ func Lock(doLock func(string, LockType) bool, doUnlock func(string, LockType) bo
 	return false
 }
 
+/*
+	Generic Unlocking function
+*/
 func Unlock(doUnlock func(string, LockType) bool, unlockNeeds []LockNeed) bool {
 	// Sanitize unlock needs to avoid deadlocks
 	sanitizedUnlockNeeds := sanitizeUnlockNeeds(unlockNeeds)
@@ -121,4 +146,61 @@ func Unlock(doUnlock func(string, LockType) bool, unlockNeeds []LockNeed) bool {
 	}
 
 	return unlockSuccess
+}
+
+/*
+	Defines an object that can be (R)locked/(R)unlocked
+*/
+type RWRecordLocker interface {
+	RLock()
+	RUnlock()
+	Lock()
+	Unlock()
+}
+
+/*
+	Generates a functor that (r)locks/(r)unlocks RWRecordLocker
+*/
+func lockingFunctorGenerator(lockType LockType, lockingType LockingType) func(memstore.Item) (memstore.Item, bool) {
+	return func(obj memstore.Item) (memstore.Item, bool) {
+		rwlocker := obj.(RWRecordLocker)
+
+		if lockType == WriteLockType {
+			if lockingType == Locking {
+				rwlocker.Lock()
+			} else {
+				rwlocker.Unlock()
+			}
+		} else {
+			if lockingType == Locking {
+				rwlocker.RLock()
+			} else {
+				rwlocker.RUnlock()
+			}
+		}
+		return obj, true
+	}
+}
+
+/*
+	Generates a functor that can be used with a generic RWRecordLocker for Lock/Unlock with Lockneeds
+*/
+func RecordLockingFunctorGenerator(
+	mem *memstore.Memstore,
+	lockingType LockingType,
+	makeSearchRecordFunctor func(string) memstore.Item,
+	indexString string,
+	save bool,
+	collectionPtr *[]memstore.Item,
+) func(string, LockType) bool {
+	return func(id string, lockType LockType) bool {
+		searchRecord := makeSearchRecordFunctor(id)
+		memstoreItem := mem.UpdateData(searchRecord, indexString, lockingFunctorGenerator(lockType, lockingType))
+		if memstoreItem == nil {
+			return false
+		} else if save {
+			*collectionPtr = append(*collectionPtr, memstoreItem)
+		}
+		return true
+	}
 }
