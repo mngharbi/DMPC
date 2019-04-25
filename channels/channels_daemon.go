de9de31a6c6f8950ca7bb07b2612d40797fb8cb5
Nizar Gharbi <email@mngharbi.com>
2019-04-25 15:38:19 -0400

    Implement add/close channel

--- channels/channels_daemon.go
+++ channels/channels_daemon.go
@@ -106,10 +106,90 @@ func (sv *channelsServer) Shutdown() error {
 	return nil
 }
 
-func (sv *channelsServer) Work(rq *gofarm.Request) (dummyReturnVal *gofarm.Response) {
+func (sv *channelsServer) Work(rqInterface *gofarm.Request) (dummyReturnVal *gofarm.Response) {
 	log.Debugf(channelsRunningRequestLogMsg)
 
-	dummyReturnVal = nil
+	var statusCode ChannelsStatusCode = ChannelsSuccess
 
-	return
+	switch (*rqInterface).(type) {
+	case *OpenChannelRequest:
+		rq := (*rqInterface).(*OpenChannelRequest)
+		channelRecord := createOrGetChannel(channelsStore, rq.Id)
+		channelRecord.Lock()
+		defer func() { channelRecord.Unlock() }()
+
+		// Try to open channel
+		actionRecord := &channelActionRecord{
+			issuerId:    rq.Signers.IssuerId,
+			certifierId: rq.Signers.CertifierId,
+			timestamp:   rq.Timestamp,
+		}
+		permissionsRecord := &channelPermissionsRecord{}
+		permissionsRecord.build(rq.Permissions)
+		openSuccess := channelRecord.tryOpen(rq.Id, actionRecord, permissionsRecord, rq.KeyId)
+		if !openSuccess {
+			statusCode = ChannelsFailure
+			break
+		}
+
+		// Feed buffered operations into decryptor
+		// @TODO: move buffer queuer outside of server object
+		// @TODO: make requests concurrently
+		channelBuffer := createOrGetChannelBuffer(bufferStore, rq.Id)
+		channelBuffer.Lock()
+		defer func() { channelBuffer.Unlock() }()
+		for _, operationPtr := range channelBuffer.operations {
+			respChannel, _ := messagesServerSingleton.operationQueuer(operationPtr)
+			_, ok := <-respChannel
+			if !ok {
+				// This occurs when decryptor is shut down prematurely
+				statusCode = BufferError
+				break
+			}
+		}
+		if statusCode == BufferError {
+			break
+		}
+
+		// Empty buffer
+		channelBuffer.operations = nil
+
+		// Notify (early) listeners of channel opening
+		notifyListeners(listenersStore, rq.Id, makeOpenEvent(channelRecord.duration.opened))
+
+		// Apply early closures
+		if channelRecord.applyCloseAttempts() {
+			notifyListeners(listenersStore, rq.Id, makeCloseEvent(channelRecord.duration.closed, 0))
+		}
+
+	case *CloseChannelRequest:
+		rq := (*rqInterface).(*CloseChannelRequest)
+
+		// Get/Lock channel
+		channelRecord := createOrGetChannel(channelsStore, rq.Id)
+		channelRecord.Lock()
+		defer func() { channelRecord.Unlock() }()
+
+		// Try to close channel
+		actionRecord := &channelActionRecord{
+			issuerId:    rq.Signers.IssuerId,
+			certifierId: rq.Signers.CertifierId,
+			timestamp:   rq.Timestamp,
+		}
+		remainingMessages, closeSuccess := channelRecord.tryClose(actionRecord)
+		if !closeSuccess {
+			statusCode = ChannelsFailure
+			break
+		}
+
+		// Only notify if channel is closed now
+		if channelRecord.state == channelClosedState {
+			notifyListeners(listenersStore, rq.Id, makeCloseEvent(channelRecord.duration.closed, remainingMessages))
+		}
+	}
+
+	var resp gofarm.Response = &ChannelsResponse{
+		Result: statusCode,
+	}
+	return &resp
 }
